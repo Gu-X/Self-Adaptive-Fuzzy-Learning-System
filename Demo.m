77440c33868a81affba037bd28d1a1cd2e030b56
XiaoweiGu <xw.gu@hotmail.com>
2021-08-07 20:56:27 +0100

    Add files via upload

--- Demo.m
+++ Demo.m
@@ -4,26 +4,20 @@ close all
 N=10;
 for ii=1:1:N
     load(['data_' num2str(ii) '.mat'])
-    seq=randperm(length(LTra1));
-    Input.x=DTra1(seq,:);Input.y=(LTra1(seq)-1.5)*2;
+    %% Vectorizing labels of training data
+    X=full(ind2vec(LTra1')');
+    X(X==0)=-1;
+    %% Training
+    Input.x=DTra1;Input.y=X;
     tic
-    [Output]=SALFS(Input,'L');
-    tt(ii)=toc
-    
+    [Output]=SAFLS(Input,'L');
+    texe(ii)=toc;
+    %% Testing
     Input.x=DTes1;Input.Syst=Output.Syst;
-    
-    [Output]=SALFS(Input,'T');
+    [Output]=SAFLS(Input,'T');
     label_est=Output.Ye;
-    label_est(Output.Ye>0)=2;
-    label_est(Output.Ye<=0)=1;
-    
-    A3(ii)=sum(sum(confusionmat(LTes1,label_est).*(eye(length(unique(LTes1))))))/length(LTes1);  
-    NN(ii)=Input.Syst.ModelNumber;
-    
+    [~,label_est]=max(label_est,[],2);
+    Acc(ii)=sum(sum(confusionmat(LTes1,label_est).*(eye(length(unique(LTes1))))))/length(LTes1);
 end
-'Training'
-'Testing'
-
-[mean(A3),std(A3),max(A3)]
-[mean(NN),std(NN),max(NN)]
-[mean(tt),std(tt),max(tt)]
+[mean(Acc),std(Acc),max(Acc)]
+[mean(texe),std(texe),max(texe)]
